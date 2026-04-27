6d72130d30ef07bfdf040cd36f7c895336d51034
samlee9898 <samyeol9898@gmail.com>
2026-04-27 18:25:04 -0500

    Setting up frontend and backend

---
+++ packages/server/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// routes
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World');
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+})
