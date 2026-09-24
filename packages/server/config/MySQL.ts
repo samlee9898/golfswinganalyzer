95f019b04cab1f9a0b017cccbe1dcb5f151bc307
samlee9898 <samyeol9898@gmail.com>
2026-09-24 00:59:58 -0500

    mysql user config change

--- packages/server/config/MySQL.ts
+++ packages/server/config/MySQL.ts
@@ -3,7 +3,7 @@ import mysql from 'mysql2/promise';
 // Create the connection to database
 const pool = mysql.createPool({
    host: `${process.env.DB_HOST}`,
-   user: 'root', //`${process.env.DB_USER}`,
+   user: `${process.env.DB_USER}`, // root
    password: `${process.env.DB_PASSWORD}`,
    database: `${process.env.DB_NAME}`,
    waitForConnections: true,
